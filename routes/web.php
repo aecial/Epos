2442bd9be9a52abd14a3aae6df7ea93828fa51bf
Teddy Pascual <teddylayson2000@gmail.com>
2026-08-28 16:44:53 +0800

    Add Back Office page and update routing; include navigation in sidebar

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('back-office', function () {
+        return Inertia::render('backOffice');
+    })->name('back-office');
 });
 
 require __DIR__.'/settings.php';
