a01538878ee4f7651ed970004595773794c1a0a1
Teddy Pascual <teddylayson2000@gmail.com>
2026-08-28 15:08:43 +0800

    Add Category and Item models with relationships; implement CategoryService and ItemService skeletons

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+class CategoryService
+{
+    public function __construct()
+    {
+        //
+    }
+    public function CreateCategory() {
+
+    }
+    public function ReadCategory() {
+
+    }
+    public function UpdateCategory() {
+    
+    }
+    public function DeleteCategory() {
+        
+    }
+}
---
+++ app/Services/ItemService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+class ItemService
+{
+    public function __construct()
+    {
+        //
+    }
+    public function CreateItem() {
+        
+    }
+    public function ReadItem() {
+
+    }
+    public function UpdateItem()  {
+
+    }
+    public function DeleteItem() {
+
+    }
+}
