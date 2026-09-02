1ef41108752c13567c6f76de9f9da5c985a2cbbc
Teddy Pascual <teddylayson2000@gmail.com>
2026-09-02 18:40:21 +0800

    Refactor ItemService methods: add parameters to CreateItem, ReadAllItem, ReadItem, UpdateItem, and DeleteItem for improved functionality and clarity.

--- app/Services/ItemService.php
+++ app/Services/ItemService.php
@@ -2,22 +2,28 @@
 
 namespace App\Services;
 
+use App\Models\Item;
+
 class ItemService
 {
     public function __construct()
     {
         //
     }
-    public function CreateItem() {
-        
+    public function CreateItem(array $itemData) {
+        return Item::create($itemData);
     }
-    public function ReadItem() {
-
+    public function ReadAllItem() {
+        return Item::all();
     }
-    public function UpdateItem()  {
-
+    public function ReadItem(Item $item) {
+        return $item;
     }
-    public function DeleteItem() {
-
+    public function UpdateItem(array $itemData, Item $item)  {
+        $item->update($itemData);
+        return $item;
+    }
+    public function DeleteItem(Item $item) {
+        return $item->delete();
     }
 }
